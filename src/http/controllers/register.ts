d9b58c6997dfb2268968ee099ffa54beeb1b9308
Gierre <gierremartins@gmail.com>
2023-12-02 16:44:02 -0300

    design patterns 02-12-2023

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,6 @@
-import { prisma } from '@/lib/prisma'
-import { FastifyRequest, FastifyReply } from 'fastify'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { RegisterUseCase } from '@/use-cases/register'
+import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -11,13 +12,18 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  await prisma.user.create({
-    data: {
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({
       name,
       email,
-      password_hash: password,
-    },
-  })
+      password,
+    })
+  } catch (error) {
+    return reply.status(409).send()
+  }
 
   return reply.status(201).send()
 }
---
+++ src/use-cases/register.ts
@@ -0,0 +1,38 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { hash } from 'bcryptjs'
+
+interface IRegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+/*
+o codigo abaixo esta estruturado, desestruturado ficaria assim:
+export async function registeUseCase(request: IRegisterUseCaseRequest) {
+  const name = request.name
+  const email = request.email
+  const password = request.password
+}
+
+(request: IRegisterUseCaseRequest), você está dizendo que a função deve receber um objeto que segue a estrutura definida por IRegisterUseCaseRequest.
+*/
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({ name, email, password }: IRegisterUseCaseRequest) {
+    const password_hash = await hash(password, 8)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new Error('email already exists')
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+  }
+}
